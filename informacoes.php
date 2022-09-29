49631c09902d048fcade4ac3d990762666626890
franciscoalecrim77 <85643940+franciscoalecrim77@users.noreply.github.com>
2022-09-29 19:24:33 -0300

    dependencias

--- informacoes.php
+++ informacoes.php
@@ -2,7 +2,7 @@
 
 //pasta apps > serve para deixar as nossas classes.
 // Pasta includes > vamos adicionar os cabeçalhos, rodapé, etc.
-
+//composer - Responsavel por fazer o autoload das classes
 
 
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'www/moboplan',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '38959279a6380e80ee729c7b8da63c8a4aba830d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'www/moboplan' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '38959279a6380e80ee729c7b8da63c8a4aba830d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
