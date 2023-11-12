c7734e0a8ff16d1ff318e17b4350f6db651b429a
franciscoalecrim77 <francisco_crim@hotmail.com>
2023-11-12 16:00:28 -0300

    Implementação dados dinamicos 1.0

--- cadastro.php
+++ cadastro.php
@@ -1,19 +1,29 @@
 <?php
 
-// include __DIR__.'/includes/headerEcletica.php';
 require_once 'vendor/autoload.php';
 
 
 setlocale(LC_ALL, "pt_BR", "pt_BR.utf-8", "portuguese");
 date_default_timezone_set('America/Sao_Paulo');
+
 if ($_SERVER['REQUEST_METHOD'] == "POST"){
 
 
 $resultado = null;
     
-$nome =      (empty($_POST['nome']))? false : $_POST['nome'];
-$datanasc =  (empty($_POST['datanasc']))? false : $_POST['datanasc'];
-$cpf =       (empty($_POST['cpf']))? false : $_POST['cpf'];
+$nome =        (empty($_POST['nome']))? false : $_POST['nome'];
+$datanasc =    (empty($_POST['datanasc']))? false : $_POST['datanasc'];
+$cpf =         (empty($_POST['cpf']))? false : $_POST['cpf'];
+$cep =         (empty($_POST['cep']))? false : $_POST['cep'];
+$rua =         (empty($_POST['rua']))? false : $_POST['rua'];
+$numero =      (empty($_POST['numero']))? false : $_POST['numero'];
+$complemento = (empty($_POST['complemento']))? false : $_POST['complemento'];
+$bairro =      (empty($_POST['bairro']))? false : $_POST['bairro'];
+$cidade =      (empty($_POST['cidade']))? false : $_POST['cidade'];
+$estado =      (empty($_POST['estado']))? false : $_POST['estado'];
+$uf =          (empty($_POST['uf']))? false : $_POST['uf'];
+
+
 
 
 $cpfajuste = preg_replace('/[^0-9]/', '',$cpf);
@@ -26,32 +36,39 @@ $cadUsuarioDao = new \app\model\cadUsuarioDao();
 $cadUsuarioDao->Validar($informacpf);
 
 foreach($cadUsuarioDao->Validar($informacpf) as $resultado):
-  //  var_dump($resultado);
 endforeach;
 
-    if(strlen($_POST['nome']) == 0){
-        echo"<script>alert('Digite o seu nome!')</script>";
-} else if (strlen($_POST['datanasc']) == 0){
+    if($resultado['cpf'] == $cpfajustado){
+        $consulta = new \app\model\cadUsuarios();
+        $consulta->setNome($resultado['nome']);
+        //echo "<script>alert('teste')</script>";
+        // echo "<pre>";
+        // print_r($consulta);
+    }
 
-    echo"<script>alert('Digite a sua data de nascimento!')</script>";}
-        if(strlen($_POST['cpf']) == ''){
-        echo"<script>alert('Digite o seu CPF!')</script>";
-} 
-               
-    else if((!empty($nome) && ($datanasc) && ($cpfajustado)) && $resultado == ''){
+    if((!empty($nome) && ($datanasc) && ($cpfajustado)) && $resultado == ''){
 
         $usuario = new \app\model\cadUsuarios();
         $usuario->setNome($nome);
         $usuario->setdataNasc($datanasc);
         $usuario->setCPF($cpfajustado);
+        $usuario->setCep($cep);
+        $usuario->setRua($rua);
+        $usuario->setNumero($numero);
+        $usuario->setComplemento($complemento);
+        $usuario->setBairro($bairro);
+        $usuario->setCidade($cidade);
+        $usuario->setEstado($estado);
+        $usuario->setUf($uf);
+       
         $cadUsuarioDao = new app\Model\cadUsuarioDao();
         $cadUsuarioDao->create($usuario);
         echo "<script>alert('Cadastro realizado com sucesso!')</script>";
-        echo "<script>alert('Voce sera redirecionado para o cadastro de credenciais')</script>";
+        //echo "<script>alert('Voce sera redirecionado para o cadastro de credenciais')</script>";
                  
 
         }else{
-            echo "Voce ja consta em nossa base de dados. Realize o acesso na tela inicial";
+            echo "<script>alert('Voce ja consta em nossa base de dados. Realize o acesso na tela inicial')</script>";
             //echo "<meta http-equiv=\"refresh\" content=\"1; url=/www\moboplan\index.php\">";   
         
         }
@@ -66,50 +83,80 @@ endforeach;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./img/favicon-32x32.png">
     <title>Cadastro</title>
     <link rel="stylesheet" href="css\cadastro.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="js/cadastro.js"></script>
+        
 </head>
 <body>
+    <script type='text/javascript'>
+            
+        
+    </script>
     <header class="header">
-     <a href="index.php"><button class=""><img  src="img/ecletica.png"></button></a>
+     
+        <img class="logo" src="img/ecletica.jpg"></img>
       
     </header>
 <section class="cadastro">
-    <form class ="formulario" action="" method="POST">
-    <div class="dados">
-        <label for=""> Seu Nome completo
-            <input type="text" name="nome" id="nome">
-        </label>
-        <label for=""> data de nascimento
-            <input type="date" name="datanasc" id="datanasc">
-        </label>
-        <label for=""> CPF
-            <input oninput="mascara(this)" type="text" name="cpf" id="cpf">
-        </label>
-    </div> 
-    <div> 
-        
-        <label for=""> 
-            <input type="submit" value="Cadastrar">
-        </label>
-    </div> 
+    <form class ="formulario" action="" method="POST" >
+
+        <fieldset class="caixaBasico">
+            <legend>Dados Básicos</legend>
+
+                <div class="dadosBasicos">
+                    <label class="labelCPF" for=""> CPF:
+                        <input class="cpf" oninput="mascara(this)" type="text" name="cpf" id="cpf" required>
+                    </label>
+                    <label class="" for=""> Nome Completo:
+                        <input class="nome" type="text" name="nome" id="nome" required>
+                    </label>
+                    <label class="" for=""> Data de Nascimento:
+                        <input class="nasc" type="date" name="datanasc" id="datanasc" required>
+                    </label>
+                </div> 
+
+        </fieldset>
+
+
+        <fieldset class="caixaEndereco">
+            <legend>Dados de endereço</legend>
+                <div class="dadosEndereco">
+                    <label class="descricao" for=""> CEP:
+                        <input class="cep" type="text" id="cep" name="cep" onblur="pesquisacep(this.value)">
+                    </label>
+                    <label class="descricao" for=""> Endereço:
+                        <input class="endereco"type="text" name="rua" id="rua">
+                    </label>
+                    <label class="descricao" for=""> Numero:
+                        <input class="numero"type="text" name="numero" id="numero">
+                    </label>
+                    <label class="descricao" for=""> Complemento:
+                        <input class="complemento" type="text" name="complemento" id="complemento">
+                    </label>
+                    <label class="descricao" for=""> Bairro:
+                        <input class="bairro" type="text" name="bairro" id="bairro">
+                    </label>
+                    <label class="descricao" for=""> Cidade:
+                        <input class="cidade"type="text" name="cidade" id="cidade">
+                    </label>
+                    <label class="descricao" for=""> Estado:
+                        <input class="estado" type="text" name="estado" id="estado">
+                    </label>
+                    <label class="descricao" for=""> UF:
+                        <input class="uf" type="text" name="uf" id="uf">
+                    </label>
+                </div> 
+        </fieldset>
+        <div class="cadastrar"> 
+                <label class="" for=""> 
+                    <input class="btn" type="submit" value="Cadastrar">
+                </label>
+        </div> 
     </form>
 </section>
 </body>
-<script>
-    function mascara(i){
-   
-   var v = i.value;
-   
-   if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
-      i.value = v.substring(0, v.length-1);
-      return;
-   }
-   
-   i.setAttribute("maxlength", "14");
-   if (v.length == 3 || v.length == 7) i.value += ".";
-   if (v.length == 11) i.value += "-";
 
-}
-</script>
 </html>
