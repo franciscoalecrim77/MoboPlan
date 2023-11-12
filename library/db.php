bd5dbd2f1f3cb2a4a29284ed5b6ee5879efb4eaa
franciscoalecrim77 <francisco_crim@hotmail.com>
2023-11-12 16:06:22 -0300

    ajuste estrutura DB

--- library/db.php
+++ library/db.php
@@ -1,17 +1,17 @@
 <?php
 
-$host = 'localhost';
-$db = "moboplan";
+$host = '127.0.0.1';
 $user = "francisco";
 $password = "weagle";
-// $port = "3306";
+$db = "eponto";
+//$port = "3308";
 
 $mysqli = new mysqli($host, $user, $password, $db);
 
 if ($mysqli->connect_errno) {
    echo "erro ao conectar ao banco de dados";
 } else {
-    echo "conectado com sucesso";
+    //echo "conectado com sucesso";
 }
 
 
